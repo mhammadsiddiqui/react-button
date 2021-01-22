7c58a61c353947dc84317e31b69a4513ffa28713
Hammad Siddiqui <mh.siddiqui89@hotmal.com>
2021-01-22 16:23:08 +0100

    not mush

--- src/RandomButton.js
+++ src/RandomButton.js
@@ -1,16 +1,16 @@
 import React from "react";
 
-const RandomButton = (props) => {
-  const { width, height, color, text } = props;
+const RandomButton = () => {
+  // const { width, height, color, text } = props;
   return (
     <button
       style={{
-        width: width || 100,
-        height: height || 100,
-        backgroundColor: color || "blue",
+        width: 100,
+        height: 80,
+        backgroundColor: "blue",
       }}
     >
-      {text}
+      {"Hello how Are you"}
     </button>
   );
 };
